be17b232b2decab58642b17460ccabc70597d3ec
Xu Lei <ray@dhcp-10-202-57-39.tamulink.tamu.edu>
2016-09-14 11:18:47 -0500

    finish sending JOIN in client

--- client.c
+++ client.c
@@ -1,5 +1,5 @@
 //The client side program for SBCP based chat program
-
+a
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,6 +9,8 @@
 #include <netinet.h>
 #include <netdb.h>
 
+#include "sbcp.h"
+
 
 int main(int argc, char *argv[])
 {
@@ -16,6 +18,15 @@ int main(int argc, char *argv[])
     char* username;
     struct hostent *server_ip;
     struct sockaddr_in server_addr;
+
+    //sbcp structure
+    struct attr_sbcp attr;
+    struct msg_sbcp msg;
+
+    //buffer for sending message payload
+    char join_buffer[20];
+    char message_buffer[512];
+    char sock_buffer[1000];
 
     if (argc < 4){
         fprintf(stderr, "usage %s username server_ip server_port", argv[0]);
@@ -31,11 +42,34 @@ int main(int argc, char *argv[])
     }
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(server_port);
-    //TODO: add server ip to the sockaddr
+    
+        
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
+    if (connect(sockfd, &server_addr, sizeof(server_addr)){
+        close(&sockfd);
+        error("Cannot connect to server! \n Exit ...");           exit (0);
+    }
 
+    //TODO: Client send JOIN msg to server
+    strcpy(join_buffer, username);
+    attr.type = ATTR_USERNAME;
+    attr.payload = join_buffer;
+    attr.length = sizeof(attr);
 
+    msg.version = 3;
+    msg.type = SBCP_JOIN;
+    msg.payload = &attr;
+    msg.length = sizeof(msg);
+
+    memcpy(sock_buffer, msg, msg.length);
+
+    if (send(sockfd, sock_buffer, sizeof(sock_buffer)) == -1){
+        fprintf(stderr, "Cannot send out JOIN message. Quit...");
+        exit(0);
+
+    }
 
     return 0;
+    }
 }
